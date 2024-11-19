944ee3643aeab6fd75c40010102d51c7f8ea6e6f
smw22 <serwes01@easv365.dk>
2024-11-19 13:55:52 +0100

    navbar with functionality (menu)

--- functions.php
+++ functions.php
@@ -6,3 +6,16 @@ function monas_register_stylesheet() {
     wp_enqueue_script("bootstrap-js", "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js");
 }
 add_action("wp_enqueue_scripts", "monas_register_stylesheet");
+
+add_theme_support( 'menus' );
+add_theme_support( 'post-thumbnails' );
+
+add_action( 'init', 'register_my_menus' );
+
+function register_my_menus() {
+    register_nav_menus(
+        array(
+            'primary-menu' => __( 'Primary Menu' ),
+        )
+    );
+}
