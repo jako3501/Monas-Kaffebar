b3a582cc4e8fe98f5f5632769fa72f1fc214d19d
jako3501 <jako3501@easv365.dk>
2024-11-20 09:29:02 +0100

    test CLI

--- home.php
+++ home.php
@@ -1,4 +1,37 @@
 <?php get_header() ?>
 
+<main>
+    <div class="blog-header">
+        <h1>Recipies</h1>
+    </div>
+
+    <section class="blog-container ">
+        <?php if (have_posts()): ?>
+            <?php while (have_posts()):
+                the_post() ?>
+                <article class="blog-content">
+
+
+                    <?php
+                    $url = get_permalink();
+                    $title = get_the_title();
+                    $date = get_the_date();
+                    $author = get_the_author();
+                    $excerpt = get_the_excerpt();
+
+                    ?>
+
+                    <?php echo get_the_post_thumbnail(get_the_ID(), 'large'); ?>
+                    <h2><a href="<?php echo $url; ?>"><?php echo $title; ?></a></h2>
+                    <p><?php echo $date; ?> by <?php echo $author; ?></p>
+                    <p><?php echo $excerpt; ?></p>
+
+
+                </article>
+            <?php endwhile; ?>
+        <?php endif; ?>
+
+    </section>
+</main>
 
 <?php get_footer() ?>
