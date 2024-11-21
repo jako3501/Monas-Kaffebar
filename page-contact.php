bf949132094411bdbf4e9f6ab3fb92e83e0d80e7
jako3501 <jako3501@easv365.dk>
2024-11-21 15:58:32 +0100

    form design

--- page-contact.php
+++ page-contact.php
@@ -7,9 +7,9 @@
 <?php get_header(); ?>
 <main>
     <h1>Lav din egen kop</h1>
-    <p>For at lave din egen kop, skal du blot tage kontakt til os ved at udfylde denne form. Derefter finder vi ud af
+    <p>For at lave din egen kop, skal du blot tage kontakt til os 
+        ved at udfylde denne form. Derefter finder vi ud af
         resten i e-mail tråden
-
     </p>
     <?php echo do_shortcode('[contact-form-7 id="f9ede22" title="Custom-cup-form"]') ?>
 </main>
