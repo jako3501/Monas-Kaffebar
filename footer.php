a98260345d53786663da3c680a5ce68f0d2bf1b0
jako3501 <jako3501@easv365.dk>
2024-11-21 14:08:39 +0100

    footer added

--- footer.php
+++ footer.php
@@ -1,8 +1,20 @@
 <?php wp_footer() ?>
 
 <footer>
+    <h4>Kontakt</h4>
+    <div class="fotter-blocks">
+        <div class="block-one">
+            <h5>E-mail</h5>
+            <p>monaskaffebar@outlook.dk</p>
+        </div>
 
+        <div class="block-two">
+            <h5>Sociale Medier</h5>
+            <div class="some-svgs">
 
+            </div>
+        </div>
+    </div>
 </footer>
 
 </body>
