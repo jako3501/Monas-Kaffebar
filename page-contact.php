661a42d47dbeb69e6922d5639f2ec61f66201e0e
jako3501 <jako3501@easv365.dk>
2024-11-21 15:29:51 +0100

    Update page-contact.php

--- page-contact.php
+++ page-contact.php
@@ -3,8 +3,8 @@
     <h1>Lav din egen kop</h1>
     <p>For at lave din egen kop, skal du blot tage kontakt til os ved at udfylde denne form. Derefter finder vi ud af
         resten i e-mail tråden
-        
+
     </p>
-    <?php echo do_shortcode('[contact-form-7 id="f9ede22" title="Custom cup form"]') ?>
+    <?php echo do_shortcode('[contact-form-7 id="f9ede22" title="Custom-cup-form"]') ?>
 </main>
 <?php get_footer ?>
