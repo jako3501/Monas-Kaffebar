8ae33bab524f4d37a460c2fcd90621efc7a038f0
jako3501 <jako3501@easv365.dk>
2024-11-26 20:31:09 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,6 @@
 <?php wp_footer() ?>
 
 <footer>
-    <h4>Kontakt</h4>
     <div class="footer-blocks">
         <div class="block-one">
             <h5>E-mail</h5>
@@ -11,19 +10,28 @@
         <div class="block-two">
             <h5>Sociale Medier</h5>
             <div class="some-svgs">
-                <svg class="facebook" xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" color="#ffffff" viewBox="0 0 24 24">
+                <svg class="facebook" xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" color="#ffffff"
+                    viewBox="0 0 24 24">
                     <path
                         d="M12 2.04C6.5 2.04 2 6.53 2 12.06C2 17.06 5.66 21.21 10.44 21.96V14.96H7.9V12.06H10.44V9.85C10.44 7.34 11.93 5.96 14.22 5.96C15.31 5.96 16.45 6.15 16.45 6.15V8.62H15.19C13.95 8.62 13.56 9.39 13.56 10.18V12.06H16.34L15.89 14.96H13.56V21.96A10 10 0 0 0 22 12.06C22 6.53 17.5 2.04 12 2.04Z" />
                 </svg>
-                <svg class="tiktok" xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" color="#ffffff" viewBox="0 0 24 24">
+                <svg class="tiktok" xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" color="#ffffff"
+                    viewBox="0 0 24 24">
                     <path fill="currentColor"
                         d="M16.6 5.82s.51.5 0 0A4.28 4.28 0 0 1 15.54 3h-3.09v12.4a2.59 2.59 0 0 1-2.59 2.5c-1.42 0-2.6-1.16-2.6-2.6c0-1.72 1.66-3.01 3.37-2.48V9.66c-3.45-.46-6.47 2.22-6.47 5.64c0 3.33 2.76 5.7 5.69 5.7c3.14 0 5.69-2.55 5.69-5.7V9.01a7.35 7.35 0 0 0 4.3 1.38V7.3s-1.88.09-3.24-1.48" />
                 </svg>
-                <svg class="instagram" xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" color="#ffffff" viewBox="0 0 24 24">
+                <svg class="instagram" xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" color="#ffffff"
+                    viewBox="0 0 24 24">
                     <path
                         d="M7.8,2H16.2C19.4,2 22,4.6 22,7.8V16.2A5.8,5.8 0 0,1 16.2,22H7.8C4.6,22 2,19.4 2,16.2V7.8A5.8,5.8 0 0,1 7.8,2M7.6,4A3.6,3.6 0 0,0 4,7.6V16.4C4,18.39 5.61,20 7.6,20H16.4A3.6,3.6 0 0,0 20,16.4V7.6C20,5.61 18.39,4 16.4,4H7.6M17.25,5.5A1.25,1.25 0 0,1 18.5,6.75A1.25,1.25 0 0,1 17.25,8A1.25,1.25 0 0,1 16,6.75A1.25,1.25 0 0,1 17.25,5.5M12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9Z" />
                 </svg>
             </div>
+        </div>
+
+        <div class="block-three">
+            <a href="<?php echo get_permalink(get_page_by_path('terms-and-conditions')); ?>">Terms and conditions</a>
+            <a href="<?php echo get_permalink(get_page_by_path('privacy-policy')); ?>">Privacy policy</a>
+            <a href="<?php echo get_permalink(get_page_by_path('refund-return')); ?>">Return and refund policy</a>
         </div>
     </div>
 </footer>
